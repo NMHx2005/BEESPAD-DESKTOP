93aac0e785c4669e4e05bb57f3ae5eeeb9b9d3f7
NMHx2005 <22092005nguyenhung@gmail.com>
2024-07-14 20:10:37 +0700

    auto run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 AOS.init();
+
+
+const marquee = document.getElementById('my-marquee');
+    setTimeout(() => {
+        marquee.stop(); // Dừng chạy
+        setTimeout(() => {
+            marquee.start(); // Bắt đầu chạy lại
+        }, 100); // Lặp lại sau 3 giây
+    }, 100); // Dừng sau 3 giây
